da111d0bd52ac2660cedecaa30b86f4a9a0eb664
Daniel Vieira <daniel2002danivieira@gmail.com>
2024-11-06 19:54:28 +0000

    UPDATE: Ajuste dos includes na biblioteca de conexão

--- ConexaoRawSocket.c
+++ ConexaoRawSocket.c
@@ -1,12 +1,4 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <net/ethernet.h>
-#include <linux/if_packet.h>
-#include <linux/if.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include "ConexaoRawSocket.h"
 
 
 int ConexaoRawSocket(char *device)
--- ConexaoRawSocket.h
+++ ConexaoRawSocket.h
@@ -1,3 +1,15 @@
-#ifndef CONEXAO_RAW_SOCKET
+#ifndef CONEXAO_RAW_SOCKET_H
+#define CONEXAO_RAW_SOCKET_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/ethernet.h>
+#include <linux/if_packet.h>
+#include <linux/if.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 int ConexaoRawSocket(char * device);
 #endif
